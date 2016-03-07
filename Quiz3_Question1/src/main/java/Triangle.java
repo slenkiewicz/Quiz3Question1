84e5115e0ed03d327c9060d43c0418d6a0918031
Steven <Steven@Steven-HP>
2016-03-07 18:11:58 -0500

    Added javadocs

--- Quiz3_Question1/src/main/java/Triangle.java
+++ Quiz3_Question1/src/main/java/Triangle.java
@@ -6,12 +6,21 @@ public class Triangle extends GeometricObject {
 	private double side2;
 	private double side3;
 
+	/**
+	 * No-arg constructor for triangle
+	 */
 	public Triangle() {
 		side1 = 1.0;
 		side2 = 1.0;
 		side3 = 1.0;
 	}
 
+	/**
+	 * @param a
+	 * @param b
+	 * @param c
+	 * Argument constructor for triangle
+	 */
 	public Triangle(double a, double b, double c) {
 		super();
 		side1 = a;
@@ -44,17 +53,26 @@ public class Triangle extends GeometricObject {
 	}
 
 	
+	/* Computes the area of a triangle
+	 * using 3 side lengths
+	 */
 	public double getArea() {
 		double halfPerim = (side1 + side2 + side3) / 2;
 		return Math.sqrt(halfPerim * (halfPerim - side1) * (halfPerim - side2) * (halfPerim - side3));
 	}
 
 	
+	/* Compute perimeter using
+	 * 3 side lengths
+	 */
 	public double getPerimeter() {
 		return side1 + side2 + side3;
 	}
 
 
+	/* Returns a string describing the triangles side lengths
+	 * 
+	 */
 	public String toString() {
 		return ("This triangle has side lengths" + side1 + "," + side2 + "," + side3);
 	}
